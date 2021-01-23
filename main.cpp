70b5a5f8d3230277a96d0ba5332f7c3b8304e588
Eric Pedley <ericpedley@gmail.com>
2021-01-23 15:04:00 -0800

    cleaned up stuff

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include <windows.h>
 #include "screencap.cpp"
 
-using std::cout;
-
 cv::Point findImageCenter(cv::Mat img, cv::Mat targetImage) {//img is the larger image and targetImage is what you want to find within img.
     cv::Size targetSize = targetImage.size();
     int width = targetSize.width;
@@ -13,7 +11,7 @@ cv::Point findImageCenter(cv::Mat img, cv::Mat targetImage) {//img is the larger
     cv::matchTemplate(img,targetImage,matchOutput,0);
     double minVal, maxVal;
     cv::Point minLoc, maxLoc;
-    cv::minMaxLoc(matchOutput.reshape(1),&minVal, &maxVal, &minLoc, &maxLoc);
+    cv::minMaxLoc(matchOutput.reshape(1),&minVal, &maxVal, &minLoc, &maxLoc);//idk why I needed matchOutput.reshape(1)
     cv::Point topLeft = minLoc;
     return topLeft + cv::Point(width/2,height/2);
 }
@@ -21,9 +19,8 @@ cv::Point findImageCenter(cv::Mat img, cv::Mat targetImage) {//img is the larger
 int main() {//you need to start the program when the zone is just created, so it looks exactly like zone.png
     HWND chromeWindow = FindWindow(NULL,"Don't let Kanye into his zone: Kanye Zone - Google Chrome");
     cv::Mat zonepic = cv::imread("zone.png");//87x87 image
-    //cv::waitKey(0);
     cv::Mat screenshot1 = captureScreenMat(chromeWindow);
-    cv::cvtColor(screenshot1,screenshot1,cv::COLOR_BGRA2BGR);//drops the alpha channel
+    cv::cvtColor(screenshot1,screenshot1,cv::COLOR_BGRA2BGR);//drops the alpha channel. it's cvtcolor instead of reshape(3) because reshape doesn't drop the alpha, it just rearranges the matrix.
     cv::Point origin = findImageCenter(screenshot1,zonepic);
     while(1) {
         cv::Mat screenshot = captureScreenMat(chromeWindow,origin.x-465/2,origin.y-466/2,465,466);
