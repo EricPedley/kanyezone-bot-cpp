a882ecf9efc31b8c665068aab7daec35addaedf4
Eric Pedley <ericpedley@gmail.com>
2021-02-07 01:44:20 -0800

    sped up screenshots

--- main.cpp
+++ main.cpp
@@ -24,11 +24,11 @@ int main() {//you need to start the program when the zone is just created, so it
     cv::Mat kanyePic = cv::imread("kanye.png");
     cv::Mat paddlePic = cv::imread("paddle.png");
     HWND chromeWindow = FindWindow(NULL,"Don't let Kanye into his zone: Kanye Zone - Google Chrome");
-    
     //figure out where the game is on the screen and save the center location
-    cv::Mat screenshot1 = captureScreenMat(chromeWindow);
+    cv::Mat screenshot1 = ScreenCapturer(chromeWindow).getScreenCap();
     cv::cvtColor(screenshot1,screenshot1,cv::COLOR_BGRA2BGR);//drops the alpha channel. it's cvtcolor instead of reshape(3) because reshape doesn't drop the alpha, it just rearranges the matrix. https://docs.opencv.org/4.1.1/d8/d01/group__imgproc__color__conversions.html
     cv::Point origin = findImageCenter(screenshot1,zonePic);
+    ScreenCapturer cap = ScreenCapturer(chromeWindow,origin.x-465/2,origin.y-466/2,465,466);
 
 
     //set up scale factor
@@ -42,7 +42,7 @@ int main() {//you need to start the program when the zone is just created, so it
         // std::cout<<float(now-prev)/CLOCKS_PER_SEC<<std::endl;
         // prev=now;
         int t1 = clock();
-        cv::Mat screenshot = captureScreenMat(chromeWindow,origin.x-465/2,origin.y-466/2,465,466);//screenshot is cropped to the game window if you started the program at the right time, when matchtemplate can find zone.png.
+        cv::Mat screenshot = cap.getScreenCap();//screenshot is cropped to the game window if you started the program at the right time, when matchtemplate can find zone.png.
         int t2 = clock();
         std::cout<<float(t2-t1)/CLOCKS_PER_SEC<<std::endl;
         cv::cvtColor(screenshot,screenshot,cv::COLOR_BGRA2BGR);//drops the alpha channel
--- screencap.cpp
+++ screencap.cpp
@@ -33,60 +33,67 @@ BITMAPINFOHEADER createBitmapHeader(int width, int height)
 	return bi;
 }
 
-/**ripped from https://superkogito.github.io/blog/CaptureScreenUsingOpenCv.html, but modified
- * Capture a screen window as a matrix.
- *
- * @param hwnd : window handle.
- *
- * @return Mat (Mat of the captured image)
- */
 using cv::Mat;
-Mat captureScreenMat(HWND hwnd, int offsetX = 0, int offsetY = 0, int cropWidth = -1, int cropHeight = -1)
-{
-	Mat src;
 
-	// get handles to a device context (DC)
-	HDC hwindowDC = GetDC(hwnd);
-	HDC hwindowCompatibleDC = CreateCompatibleDC(hwindowDC);
-	SetStretchBltMode(hwindowCompatibleDC, COLORONCOLOR);
+class ScreenCapturer {
+	HWND hwnd;
+	int height, width, screenx, screeny;
+	HDC hwindowDC,hwindowCompatibleDC;
+	BITMAPINFOHEADER bi;
+	HBITMAP hbwindow;
+	Mat src;
+	public:
+		ScreenCapturer(HWND, int, int, int, int);
+		~ScreenCapturer();
+		Mat getScreenCap();
+};
+
+ScreenCapturer::ScreenCapturer(HWND hwndParameter, int offsetX = 0, int offsetY = 0, int cropWidth = -1, int cropHeight = -1) {
+	hwnd=hwndParameter;
+	hwindowDC = GetDC(hwnd);
+	hwindowCompatibleDC = CreateCompatibleDC(hwindowDC);
+	//SetStretchBltMode(hwindowCompatibleDC, COLORONCOLOR);
 
 	// define x,y,height and width
 	RECT r;
 	GetWindowRect(hwnd, &r);	   //passes window handle and pointer to r, and this function modifies r.
-	int screenx = r.left+offsetX;		   //GetSystemMetrics(SM_XVIRTUALSCREEN);
-	int screeny = r.top+offsetY;		   //GetSystemMetrics(SM_YVIRTUALSCREEN);
-	int width;  //GetSystemMetrics(SM_CXVIRTUALSCREEN);
+	screenx = r.left+offsetX;		   //GetSystemMetrics(SM_XVIRTUALSCREEN);
+	screeny = r.top+offsetY;		   //GetSystemMetrics(SM_YVIRTUALSCREEN);
+	width;  //GetSystemMetrics(SM_CXVIRTUALSCREEN);
 	if(cropWidth==-1)
 		width = r.right - r.left;  
 	else
 		width = cropWidth;
-	int height;
+	height;
 	if(cropHeight==-1)
 		height = r.bottom - r.top; //GetSystemMetrics(SM_CYVIRTUALSCREEN);
 	else
 		height=cropHeight;
+	bi = createBitmapHeader(width, height);
+	// create a bitmap
+	hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
+	// use the previously created device context with the bitmap
+	SelectObject(hwindowCompatibleDC, hbwindow);
 	// create mat object
 	src.create(height, width, CV_8UC4);
 
-	// create a bitmap
-	HBITMAP hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
-	BITMAPINFOHEADER bi = createBitmapHeader(width, height);
+}
 
-	// use the previously created device context with the bitmap
-	SelectObject(hwindowCompatibleDC, hbwindow);
+ScreenCapturer::~ScreenCapturer() {
+	// avoid memory leak
+	DeleteObject(hbwindow);
+	DeleteDC(hwindowCompatibleDC);
+	ReleaseDC(hwnd, hwindowDC);
+}
 
-	// copy from the window device context to the bitmap device context
+Mat ScreenCapturer::getScreenCap()
+{
 	//This is what the problem was. stretchblt was producing a completely black image so I replaced it with bitblt and now it works
 	//StretchBlt(hwindowCompatibleDC, 0, 0, width, height, hwindowDC, screenx, screeny, width, height, SRCCOPY);  //change SRCCOPY to NOTSRCCOPY for wacky colors !
 	BitBlt(hwindowCompatibleDC, 0, 0, width, height, GetDC(0), screenx, screeny, SRCCOPY);
 
 	//this line is what copies the hbitmap image information to the opencv matrix object
 	GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, src.data, (BITMAPINFO *)&bi, DIB_RGB_COLORS); //copy from hwindowCompatibleDC to hbwindow
-
-	// avoid memory leak
-	DeleteObject(hbwindow);
-	DeleteDC(hwindowCompatibleDC);
-	ReleaseDC(hwnd, hwindowDC);
 
 	return src;
 }
